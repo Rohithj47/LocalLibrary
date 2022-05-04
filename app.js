c314506b5cea550e175f3829b3f7a4fd364ab563
Rohithj47 <rohithjanardhan98@gmail.com>
2022-05-04 22:35:41 +0530

    Mongo URL fix on heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-require('dotenv').config()
+var compress = require('compression')
+var helmet = require('helmet')
 
 
 var indexRouter = require('./routes/index');
@@ -25,6 +27,9 @@ mongoose.connect(
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+app.use(helmet())
+// Setup compression for Responses 
+app.use(compress())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
